f3c515aece33832bde99e63fb60bab7aeff729c8
sprajapati123 <sandisprajapati52@gmail.com>
2025-01-17 08:16:11 +0545

    repo

--- app/src/main/java/com/example/c35d_crud/repository/ProductRepositoryImpl.kt
+++ app/src/main/java/com/example/c35d_crud/repository/ProductRepositoryImpl.kt
@@ -1,8 +1,11 @@
 package com.example.c35d_crud.repository
 
 import com.example.c35d_crud.model.ProductModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class ProductRepositoryImpl : ProductRepository {
 
@@ -47,7 +50,6 @@ class ProductRepositoryImpl : ProductRepository {
                 callback(true,"Product deleted successfully")
             }else{
                 callback(false,"${it.exception?.message}")
-
             }
         }
     }
@@ -56,10 +58,39 @@ class ProductRepositoryImpl : ProductRepository {
         productId: String,
         callback: (ProductModel?, Boolean, String) -> Unit
     ) {
-        TODO("Not yet implemented")
+        ref.child(productId).addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+              if(snapshot.exists()){
+                  var model = snapshot.getValue(ProductModel::class.java)
+                  callback(model,true,"Product fetched")
+              }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                callback(null,false,error.message)
+            }
+        })
     }
 
     override fun getAllProducts(callback: (List<ProductModel>?, Boolean, String) -> Unit) {
-        TODO("Not yet implemented")
+        ref.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    var products = mutableListOf<ProductModel>()
+                    for(eachData in snapshot.children){
+                        var model = eachData.getValue(ProductModel::class.java)
+                       if(model != null){
+                           products.add(model)
+                       }
+                    }
+
+                    callback(products,true,"Product fetched")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                callback(null,false,error.message)
+            }
+        })
     }
 }
